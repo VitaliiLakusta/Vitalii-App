0e586aca168001922f8287a4041e1408ef92457f
Vitalii Lakusta <lakusta96@gmail.com>
2015-03-17 15:05:01 +0200

    last stage of what I did in the class

---
+++ Vitalii App/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Vitalii App
+//
+//  Created by kasutaja on 12.03.15.
+//  Copyright (c) 2015 kasutaja. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Vitalii App/ViewController.swift
+++ Vitalii App/ViewController.swift
@@ -1,4 +1,4 @@
-//
+    //
 //  ViewController.swift
 //  Vitalii App
 //
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passText: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func verifyUser(sender: AnyObject) {
+        if (usernameText.text != "admin" || passText.text != "admin") {
+            // display warning
+        }
+        else {
+            self.performSegueWithIdentifier("segueToDetailsTableView", sender:self)
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
